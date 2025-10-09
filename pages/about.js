f3f2678065975c1888f070e85b98610d9011030b
Brennen Adams <158304197+Bcolea13@users.noreply.github.com>
2025-10-08 23:35:16 -0400

    Create about.js

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import NavBar from "../components/NavBar";
+
+export default function About() {
+  return (
+    <>
+      <NavBar />
+      <main className="container">
+        <h1>About Me</h1>
+        <p>
+          I’m Brennen Adams — a photographer and computer engineering major at the University of Kentucky.
+          I combine art and technology through computational photography and digital imaging.
+        </p>
+      </main>
+    </>
+  );
+}
