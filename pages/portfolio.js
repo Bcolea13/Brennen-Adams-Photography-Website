ac62a6831fc9e1f0638cec2d1f1a4c33bfd3561b
Brennen Adams <158304197+Bcolea13@users.noreply.github.com>
2025-10-08 23:35:34 -0400

    Create portfolio.js

---
+++ pages/portfolio.js
@@ -0,0 +1,14 @@
+import NavBar from "../components/NavBar";
+
+export default function Portfolio() {
+  return (
+    <>
+      <NavBar />
+      <main className="container">
+        <h1>Portfolio</h1>
+        <p>Here are some of my favorite shoots and creative projects.</p>
+        {/* Later, we can add a photo grid or gallery here */}
+      </main>
+    </>
+  );
+}
