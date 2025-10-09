940767d0d2d06893b050fe0b399561e7d115d0fe
Brennen Adams <158304197+Bcolea13@users.noreply.github.com>
2025-10-09 16:23:42 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
+import NavBar from "../components/NavBar";
 
 export default function Home() {
   return (
