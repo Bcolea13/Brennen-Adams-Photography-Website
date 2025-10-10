0583c0d8aa3a392d3a8455bb105395d1e70982fa
Brennen Adams <158304197+Bcolea13@users.noreply.github.com>
2025-10-10 14:30:18 -0400

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,6 @@
-//import NavBar from "../components/NavBar";
-
 export default function About() {
   return (
     <>
-      //<NavBar />
-      //<main className="container">
         <h1>About Me</h1>
         <p>
           Hello.
