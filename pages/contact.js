da12005ca134c4800a06ba6950b3aa27f9fb70c1
Brennen Adams <158304197+Bcolea13@users.noreply.github.com>
2025-10-10 14:49:27 -0400

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,8 @@
-import NavBar from "../components/NavBar";
-
 export default function Contact() {
   return (
-    <>
-      <NavBar />
-      <main className="container">
-        <h1>Contact</h1>
+      <main>
+        <h1>Contact
+        </h1>
         <p>
           Email me at:{" "}
           <a href="mailto:hello@brennenadamsphotography.com">
